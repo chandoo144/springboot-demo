266a576066b47f8af639f336996f773b22212a3a
chandooiitm <chandoo_iitm@yahoo.co.in>
2024-03-01 07:41:44 +0530

    initial commit

---
+++ src/main/java/com/chandoo1441/springbootdemo/gitdemo/DataforGitDemo.java
@@ -0,0 +1,12 @@
+package com.chandoo1441.springbootdemo.gitdemo;
+
+public class DataforGitDemo {
+    public static void main(String[] args) {
+        System.out.println("This a test to check the file in git..");
+    }
+
+    public static String getName() {
+        String myname = name;
+        return myname;
+    }
+}
---
+++ src/test/java/com/chandoo1441/springbootdemo/gitdemo/DataforGitDemoTest.java
@@ -0,0 +1,15 @@
+package com.chandoo1441.springbootdemo.gitdemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataforGitDemoTest {
+
+    @Test
+    public void getName() {
+     //  assertThat.assertEquals("chandoo1441", DataforGitDemo.getName());
+        System.out.println("this is a test method to check test class");
+
+    }
+}
