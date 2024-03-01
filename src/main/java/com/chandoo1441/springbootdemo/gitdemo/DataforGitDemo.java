c8b3c0389681698f12ba70d5e9b87f8673b0ec8a
chandooiitm <chandoo_iitm@yahoo.co.in>
2024-03-01 08:02:34 +0530

    initial commit 2

--- src/main/java/com/chandoo1441/springbootdemo/gitdemo/DataforGitDemo.java
+++ src/main/java/com/chandoo1441/springbootdemo/gitdemo/DataforGitDemo.java
@@ -3,6 +3,7 @@ package com.chandoo1441.springbootdemo.gitdemo;
 public class DataforGitDemo {
     public static void main(String[] args) {
         System.out.println("This a test to check the file in git..");
+        // adding a comment here for git
     }
 
     public static String getName() {
